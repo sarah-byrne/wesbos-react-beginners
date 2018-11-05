99b5b44578d23067abee2b3de7a456e8a0d0277f
sarah-byrne <sarah.byrne@johnlewis.co.uk>
2018-11-05 12:01:00 +0000

    Add code for up to end of video 9

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header />
+                    <Header tagline="Fresh seafood market"/>
                 </div>
                 <Order />
                 <Inventory />
