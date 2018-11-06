4f3c68392bdec9e066e1ef716f493a7fcd0a87da
sarah-byrne <sarah.byrne@johnlewis.co.uk>
2018-11-06 17:43:30 +0000

    Add code up to end of video 10

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
     return (
         <form className="store-selector">
             <h2>Please enter a store</h2>
-            <input type="text" required placeholder="store name" />
+            <input type="text" required placeholder="store name" defaultValue={getFunName()} />
             <button type="submit">Visit</button>
         </form>
     )
